e2f2ca908baee48eccf4f43b79e2592f83d8653b
Adam Grant <ajkochanowicz@gmail.com>
2016-12-12 11:05:35 -0800

    Update kickstart.js

--- kickstart.js
+++ kickstart.js
@@ -1,22 +1,5 @@
-// Declaration
-k$ = {
-  $: function(el) {
-    return this.$$(el)[0];
-  },
-  $$: function(el) {
-    return document.querySelectorAll(el);
-  },
-  component: function(selectors, fn) {
-    var selector = [""].concat((selectors).split(" ")).reduce(function(a, b) {
-      return a + '[data-component~="' + b + '"]'
-    });
-    var $selector = k$.$(selector),
-        getInScope = function(scoped_selector) {
-          return $selector.querySelector(scoped_selector);
-        },
-        getInScopeMultiple = function(scoped_selector) {
-          return $selector.querySelectorAll(scoped_selector);
-        };
-    return fn.call(this, getInScope, getInScopeMultiple);
-  }
-};
+// This file requires Mozart to be loaded first
+// https://github.com/adamjgrant/Mozart
+
+m$.ks_navbar = new Mozart();
+m$.ks_modal  = new Mozart();
